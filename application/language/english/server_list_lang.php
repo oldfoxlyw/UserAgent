15f3dbb19ee878cacea52bf803b9e0becbc7d8f4
johnnyeven_work <johnnyeven@gmail.com>
2013-03-01 13:53:28 +0800

    null

--- application/language/english/server_list_lang.php
+++ application/language/english/server_list_lang.php
@@ -3,6 +3,7 @@ $lang['server_list_A0']			=	'Doom';
 $lang['server_list_A1']			=	'Dawn';
 $lang['server_list_A2']			=	'Daylight';
 $lang['server_list_GX']			=	'Tarre';
+$lang['server_list_Z1']			=	'Test';
 
 $lang['server_list_language_CN']= 'Chinese';
 $lang['server_list_language_EN']= 'English';
