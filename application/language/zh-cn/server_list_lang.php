1933335a9125b0c7d774613be71a975641aa05cc
johnnyeven_work <johnnyeven@gmail.com>
2013-03-25 11:16:15 +0800

    null

--- application/language/zh-cn/server_list_lang.php
+++ application/language/zh-cn/server_list_lang.php
@@ -2,6 +2,7 @@
 $lang['server_list_A0']			=	'末日';
 $lang['server_list_A1']			=	'晨曦';
 $lang['server_list_A2']			=	'曙光';
+$lang['server_list_A4']			=	'昭阳';
 $lang['server_list_F1']			=	'玛雅';
 $lang['server_list_Z1']			=	'测试';
 
