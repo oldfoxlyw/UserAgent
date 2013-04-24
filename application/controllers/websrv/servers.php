2800c68be6eca632d2dd8f8ed649725966ee36a0
johnnyeven_work <johnnyeven@gmail.com>
2013-04-24 10:12:36 +0800

    -

--- application/controllers/websrv/servers.php
+++ application/controllers/websrv/servers.php
@@ -20,16 +20,16 @@ class Servers extends CI_Controller {
 		$authKey	=	$this->config->item('game_auth_key');
 		$authToken	=	$authKey[$gameId]['auth_key'];
 
-		$status = $this->status->read();
-		if($status->server_status != '1')
-		{
-			$jsonData = array(
-				'message'	=>	'SERVER_CLOSED',
-				'text'		=>	$status->message
-			);
-			echo $this->return_format->format($jsonData, $format);
-			exit();
-		}
+		// $status = $this->status->read();
+		// if($status->server_status != '1')
+		// {
+		// 	$jsonData = array(
+		// 		'message'	=>	'SERVER_CLOSED',
+		// 		'text'		=>	$status->message
+		// 	);
+		// 	echo $this->return_format->format($jsonData, $format);
+		// 	exit();
+		// }
 		
 		if($sectionId===FALSE || empty($sectionId)) {
 			$sectionId	=	$this->config->item('game_section_id');
