a383e7cac40b0604cdb0006eeaf3a796f93fddfb
johnnyeven_work <johnnyeven@gmail.com>
2013-04-25 14:22:49 +0800

    -

---
+++ application/controllers/websrv/account2.php
@@ -0,0 +1,19 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Account2 extends CI_Controller {
+	private $root_path = null;
+	private $authKey = null;
+	
+	public function __construct() {
+		parent::__construct();
+	}
+
+	public function index()
+	{
+		echo '111';
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
+?>
