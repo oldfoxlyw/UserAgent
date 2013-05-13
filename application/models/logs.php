68e92d9b0b8fe007066b6b4153126159c8a0ce20
johnnyeven_work <johnnyeven@gmail.com>
2013-05-13 14:00:44 +0800

    1

--- application/models/logs.php
+++ application/models/logs.php
@@ -3,7 +3,6 @@ if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 class logs extends CI_Model {
 	private $accountTable = 'log_account';
-	private $paymentTable = 'log_payment';
 	private $logdb = null;
 	
 	public function __construct() {
@@ -15,47 +14,21 @@ class logs extends CI_Model {
 		if(!empty($parameter) && !empty($parameter['log_action'])) {
 			$uri					=	$this->input->server('REQUEST_URI');
 			$relativeMethod			=	$this->input->server('REQUEST_METHOD');
+			$requestIp				=	$this->input->ip_address();
 			$relativeParameter		=	json_encode($_REQUEST);
 			$currentTime			=	time();
 			$currentTimeLocal		=	date("Y-m-d H:i:s", $currentTime);
-			
-			$logAction = $parameter['log_action'];
-			$logActionArg = explode('_', $logAction);
-			switch($logActionArg[0]) {
-				case 'ACCOUNT':
-					$row = array(
-						'log_GUID'				=>	$parameter['account_guid'],
-						'log_account_name'		=>	$parameter['account_name'],
-						'log_account_email'		=>	'',
-						'log_action'			=>	$logAction,
-						'log_uri'				=>	$uri,
-						'log_method'			=>	$relativeMethod,
-						'log_parameter'			=>	$relativeParameter,
-						'log_time'				=>	$currentTime,
-						'log_time_local'		=>	$currentTimeLocal,
-						'game_id'				=>	empty($parameter['game_id']) ? '' : $parameter['game_id'],
-						'section_id'				=>	empty($parameter['section_id']) ? '' : $parameter['section_id'],
-						'server_id'				=>	empty($parameter['server_id']) ? '' : $parameter['server_id']
-					);
-					$this->logdb->insert($this->accountTable, $row);
-					break;
-				case 'PAYMENT':
-					$row = array(
-						'log_GUID'				=>	$parameter['account_guid'],
-						'log_action'			=>	$logAction,
-						'log_uri'					=>	$uri,
-						'log_method'			=>	$relativeMethod,
-						'log_parameter'		=>	$relativeParameter,
-						'log_time'				=>	$currentTime,
-						'log_time_local'		=>	$currentTimeLocal,
-						'log_ip'					=>	$this->input->ip_address(),
-						'game_id'				=>	empty($parameter['game_id']) ? '' : $parameter['game_id'],
-						'section_id'				=>	empty($parameter['section_id']) ? '' : $parameter['section_id'],
-						'server_id'				=>	empty($parameter['server_id']) ? '' : $parameter['server_id']
-					);
-					$this->logdb->insert($this->paymentTable, $row);
-					break;
-			}
+			$row = array(
+				'log_GUID'				=>	$parameter['account_guid'],
+				'log_account_name'		=>	$parameter['account_name'],
+				'log_action'			=>	$parameter['log_action'],
+				'log_parameter'			=>	$relativeParameter,
+				'log_time_local'		=>	$currentTimeLocal,
+				'log_ip'				=>	$requestIp,
+				'server_id'				=>	empty($parameter['server_id']) ? '' : $parameter['server_id']
+			);
+			$this->logdb->insert($this->accountTable, $row);
+			break;
 		}
 	}
 }
