c4edbf60b1b403e23fa9763fbac6fea1b31d4ce8
johnnyeven_work <johnnyeven@gmail.com>
2013-05-13 14:52:19 +0800

    1

--- application/models/logs.php
+++ application/models/logs.php
@@ -3,6 +3,7 @@ if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 class logs extends CI_Model {
 	private $accountTable = 'log_account';
+	private $paymentTable = 'log_payment';
 	private $logdb = null;
 	
 	public function __construct() {
@@ -14,23 +15,25 @@ class logs extends CI_Model {
 		if(!empty($parameter) && !empty($parameter['log_action'])) {
 			$uri					=	$this->input->server('REQUEST_URI');
 			$relativeMethod			=	$this->input->server('REQUEST_METHOD');
-			$requestIp				=	$this->input->ip_address();
 			$relativeParameter		=	json_encode($_REQUEST);
 			$currentTime			=	time();
 			$currentTimeLocal		=	date("Y-m-d H:i:s", $currentTime);
-			$row = array(
-				'log_GUID'				=>	$parameter['account_guid'],
-				'log_account_name'		=>	$parameter['account_name'],
-				'log_action'			=>	$parameter['log_action'],
-				'log_parameter'			=>	$relativeParameter,
-				'log_time_local'		=>	$currentTimeLocal,
-				'log_ip'				=>	$requestIp,
-				'server_id'				=>	empty($parameter['server_id']) ? '' : $parameter['server_id']
-			);
-			var_dump($row);
-			//exit();
-			$this->logdb->insert($this->accountTable, $row);
-			break;
+			
+			$logAction = $parameter['log_action'];
+			$logActionArg = explode('_', $logAction);
+			switch($logActionArg[0]) {
+				case 'ACCOUNT':
+					$row = array(
+						'log_GUID'				=>	$parameter['account_guid'],
+						'log_account_name'		=>	$parameter['account_name'],
+						'log_action'			=>	$logAction,
+						'log_parameter'			=>	$relativeParameter,
+						'log_time_local'		=>	$currentTimeLocal,
+						'server_id'				=>	empty($parameter['server_id']) ? '' : $parameter['server_id']
+					);
+					$this->logdb->insert($this->accountTable, $row);
+					break;
+			}
 		}
 	}
 }
