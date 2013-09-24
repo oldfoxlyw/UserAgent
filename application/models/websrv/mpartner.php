522290cd9b13644881b76c4c9ae8032ded2e249b
johnnyeven_work <johnnyeven@gmail.com>
2013-09-24 10:00:20 +0800

    1

--- application/models/websrv/mpartner.php
+++ application/models/websrv/mpartner.php
@@ -3,19 +3,19 @@ if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 class Mpartner extends CI_Model {
 	private $accountTable = 'scc_partner';
-	private $productdb = null;
+	private $webdb = null;
 	
 	public function __construct() {
 		parent::__construct();
-		$this->productdb = $this->load->database('webdb', true);
+		$this->webdb = $this->load->database('webdb', true);
 	}
 	
 	public function getTotal($parameter = null) {
-		return $this->productdb->count_all_results($this->accountTable);
+		return $this->webdb->count_all_results($this->accountTable);
 	}
 	
 	public function getAllResult($parameter = null) {
-		$result = $this->productdb->get($this->accountTable);
+		$result = $this->webdb->get($this->accountTable);
 		if($result->num_rows() > 0)
 		{
 			return $result->result();
