fd8b9687e793cc7db8f21c26e78435ee265858e4
johnnyeven_work <johnnyeven@gmail.com>
2013-10-09 13:37:16 +0800

    1

--- application/controllers/websrv/orders.php
+++ application/controllers/websrv/orders.php
@@ -127,6 +127,7 @@ class Orders extends CI_Controller {
 				$parameter = array(
 					'player_id'						=>	$playerId,
 					'role_id'						=>	$roleId,
+					'role_level'					=>	intval($roleLevel),
 					'action_name'				=>	$actionName,
 					'current_special_gold'	=>	$currentSpecialGold,
 					'spend_special_gold'		=>	$spendSpecialGold,
