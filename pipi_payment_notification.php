76429b601b6bbcb1185952196ba6e6c842f87a5c
johnnyeven_work <johnnyeven@gmail.com>
2013-11-15 17:27:51 +0800

    1

--- pipi_payment_notification.php
+++ pipi_payment_notification.php
@@ -13,11 +13,6 @@ if(!empty($post))
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 	curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
 		
-	if($this->enableSSL) {
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-	}
-		
 	$monfd = curl_exec($ch);
 		
 	curl_close($ch);
