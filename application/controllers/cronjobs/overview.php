869f661b2d7842a4de46ce274cc9de49fa2a624c
johnnyeven_work <johnnyeven@gmail.com>
2013-11-25 10:32:31 +0800

    1

--- application/controllers/cronjobs/overview.php
+++ application/controllers/cronjobs/overview.php
@@ -405,6 +405,13 @@ class Overview extends CI_Controller
 					//昨天登录
 					$sql = "SELECT `log_GUID` FROM `log_account` WHERE `server_id`='{$row->account_server_id}' AND `partner_key`='{$partnerKey}' AND `log_action`='ACCOUNT_LOGIN_SUCCESS' AND `log_time`>={$prevTimeStart} AND `log_time`<={$prevTimeEnd} AND `log_GUID` in (SELECT `GUID` FROM `agent1_account_db`.`web_account` WHERE `server_id`='{$row->account_server_id}' AND `partner_key`='{$partnerKey}' AND `account_regtime`>={$thirdTimeStart} AND `account_regtime`<={$thirdTimeEnd} AND `account_level`>1) GROUP BY `log_GUID`";
 					$prevCurrentLoginResult = $this->logdb->query($sql)->result();
+					
+					var_dump($thirdCurrentLoginResult);
+					
+					var_dump($prevCurrentLoginResult);
+					
+					exit();
+					
 					$thirdRetentionRange = array_intersect($thirdCurrentLoginResult, $prevCurrentLoginResult);
 					$thirdRetentionRange = count($thirdRetentionRange);
 					
