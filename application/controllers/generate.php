67e7c40000a5c0940ae3212752de9c9fbb095e79
johnnyeven_work <johnnyeven@gmail.com>
2013-12-18 14:15:16 +0800

    1

--- application/controllers/generate.php
+++ application/controllers/generate.php
@@ -1,12 +1,52 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
 
-class Generate extends CI_Controller {
+if (! defined ( 'BASEPATH' ))
+	exit ( 'No direct script access allowed' );
+
+class Generate extends CI_Controller
+{
 	private $root_path = null;
 	
-	public function index() {
-		$this->load->helper('security');
-		$this->load->library('Guid');
-		echo do_hash($this->guid->toString());
+	public function index()
+	{
+		$this->load->helper ( 'security' );
+		$this->load->library ( 'Guid' );
+		echo do_hash ( $this->guid->toString () );
+	}
+	
+	public function code()
+	{
+		$prefix = $this->input->get ( 'prefix', TRUE ); // 激活码前缀
+		$limit = $this->input->get ( 'limit', TRUE ); // 激活码的位数
+		$count = $this->input->get ( 'count', TRUE ); // 激活码个数
+		
+		$numberPool = array (
+				'0',
+				'1',
+				'2',
+				'3',
+				'4',
+				'5',
+				'6',
+				'7',
+				'8',
+				'9' 
+		);
+		
+		$prefix = empty ( $prefix ) ? '' : $prefix;
+		$limit = empty ( $limit ) ? 8 : intval ( $limit );
+		$count = empty ( $count ) ? 100 : intval ( $count );
+		
+		for($i = 0; $i < $count; $i++)
+		{
+			$code = '';
+			for($j = 0; $j < $limit; $j++)
+			{
+				$code .= $numberPool[rand(0, 9)];
+			}
+			
+			echo $code . '<br>';
+		}
 	}
 }
 
