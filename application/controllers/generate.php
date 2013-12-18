d14bdf6879cb145f4ffe9f8544309a10316eae4b
johnnyeven_work <johnnyeven@gmail.com>
2013-12-18 18:14:04 +0800

    1

--- application/controllers/generate.php
+++ application/controllers/generate.php
@@ -19,6 +19,9 @@ class Generate extends CI_Controller
 		$prefix = $this->input->get ( 'prefix', TRUE ); // 激活码前缀
 		$limit = $this->input->get ( 'limit', TRUE ); // 激活码的位数
 		$count = $this->input->get ( 'count', TRUE ); // 激活码个数
+		$comment = $this->input->get( 'comment', TRUE );
+		
+		$this->load->model('mcode');
 		
 		$numberPool = array (
 				'0',
@@ -36,15 +39,34 @@ class Generate extends CI_Controller
 		$prefix = empty ( $prefix ) ? '' : $prefix;
 		$limit = empty ( $limit ) ? 8 : intval ( $limit );
 		$count = empty ( $count ) ? 100 : intval ( $count );
+		$comment = empty( $comment ) ? '' : $comment;
 		
-		for($i = 0; $i < $count; $i++)
+		if(!empty($prefix))
+		{
+			$prefixLength = strlen($prefix);
+		}
+		else
+		{
+			$prefixLength = 0;
+		}
+		
+		for($i = 0; $i < $count - $prefixLength; $i++)
 		{
 			$code = '';
 			for($j = 0; $j < $limit; $j++)
 			{
 				$code .= $numberPool[rand(0, 9)];
 			}
+			$code = $prefix . $code;
 			
+			$parameter = array(
+					'code'		=>	$code,
+					'comment'	=>	$comment
+			);
+			if(!$this->mcode->create($parameter))
+			{
+				$i--;
+			}
 		}
 	}
 }
