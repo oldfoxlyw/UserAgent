f5d426916849a74b20a7fb25db82d00fbe4b75e3
johnnyeven_work <johnnyeven@gmail.com>
2013-12-20 14:06:59 +0800

    1

--- application/controllers/webapi/servers.php
+++ application/controllers/webapi/servers.php
@@ -8,7 +8,7 @@ class Servers extends CI_Controller {
 	}
 	
 	public function server_list($format = 'json') {
-		$serverId = $this->input->get_post('server_id', TRUE);
+		$serverId = $this->input->post('server_id', TRUE);
 		
 		if(!empty($serverId))
 		{
@@ -22,29 +22,6 @@ class Servers extends CI_Controller {
 			{
 				$this->load->helper('array');
 				$result = $result[0];
-				
-				$result->server_ip = json_decode($result->server_ip);
-				if(count($result->server_ip) > 0)
-				{
-					$result->server_ip = random_element($result->server_ip);
-				}
-				else
-				{
-					$result->server_ip = $result->server_ip[0];
-				}
-				$result->server_ip = $result->server_ip->ip . ':' . $result->server_ip->port;
-	
-				$result->server_game_ip = json_decode($result->server_game_ip);
-				if(count($result->server_game_ip) > 0)
-				{
-					$result->server_game_ip = random_element($result->server_game_ip);
-				}
-				else
-				{
-					$result->server_game_ip = $result->server_game_ip[0];
-				}
-				$result->server_game_port = $result->server_game_ip->port;
-				$result->server_game_ip = $result->server_game_ip->ip;
 				
 				$result->game_message_ip = json_decode($result->game_message_ip);
 				if(count($result->game_message_ip) > 0)
