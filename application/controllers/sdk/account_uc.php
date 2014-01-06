049595a16b5a9157961f8dda3d6de690df3f34bd
johnnyeven_work <johnnyeven@gmail.com>
2014-01-06 17:26:20 +0800

    1

--- application/controllers/sdk/account_uc.php
+++ application/controllers/sdk/account_uc.php
@@ -63,8 +63,8 @@ class Account_uc extends CI_Controller
 					$game = array(
 							'cpId'		=>	$this->cpId,
 							'gameId'	=>	$this->gameId,
-							'channelId'	=>	'JY_2',
-							'serverId'	=>	1001
+							'channelId'	=>	'',
+							'serverId'	=>	0
 					);
 					$sign = md5("{$this->cpId}{$paramStr}{$this->apiKey}");
 					$header = array(
