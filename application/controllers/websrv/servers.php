6cbe764aff83c34598ba5cfcb11729598e6537a4
johnnyeven_work <johnnyeven@gmail.com>
2014-03-05 09:02:43 +0800

    1

--- application/controllers/websrv/servers.php
+++ application/controllers/websrv/servers.php
@@ -12,7 +12,7 @@ class Servers extends CI_Controller {
 	public function server_list($format = 'json')
 	{
 		$jsonData = Array(
-				'errors'			=>	'《冰火王座》全服例行维护中，预计将于11:00结束，由此给各位玩家造成的不便，我们深表歉意！'
+				'errors'			=>	'《冰火王座》全服停机维护中，预计将于11:00结束，由此给各位玩家造成的不便，我们深表歉意！'
 		);
 		echo $this->return_format->format($jsonData, $format);
 		exit();
