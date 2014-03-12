e98b088bd013abab1c01276d3267511435c3191b
johnnyeven_work <johnnyeven@gmail.com>
2014-03-12 11:37:44 +0800

    1

--- application/controllers/websrv/servers.php
+++ application/controllers/websrv/servers.php
@@ -100,7 +100,10 @@ class Servers extends CI_Controller {
 		$productdb = $this->load->database('productdb', TRUE);
 		$sql = "SELECT `server_id` FROM `server_balance_check` WHERE `next_active` = 1";
 		$next = $productdb->query($sql)->row();
+		$maxCount = intval($next->max_count);
+		$count = intval($next->count);
 		$next = intval($next->server_id);
+
 
 		$this->load->helper('array');
 		for($i = 0; $i<count($jsonData['server']); $i++)
@@ -112,56 +115,63 @@ class Servers extends CI_Controller {
 		}
 		$jsonData['server'][$next]['server_recommend'] = 1;
 		
-		//3\4
-// 		if($next >= 1)
-// 		{
-// 			$nextServer = 0;
-// 		}
-// 		else
-// 		{
-// 			$nextServer = $next + 1;
-// 		}
-		//3\4\5
-// 		if($next >= 2)
-// 		{
-// 			$nextServer = 0;
-// 		}
-// 		else
-// 		{
-// 			$nextServer = $next + 1;
-// 		}
-		//3\4\5\6
-		if($next >= 7)
+		if($count >= $maxCount)
 		{
-			$nextServer = 4;
+			//3\4
+	// 		if($next >= 1)
+	// 		{
+	// 			$nextServer = 0;
+	// 		}
+	// 		else
+	// 		{
+	// 			$nextServer = $next + 1;
+	// 		}
+			//3\4\5
+	// 		if($next >= 2)
+	// 		{
+	// 			$nextServer = 0;
+	// 		}
+	// 		else
+	// 		{
+	// 			$nextServer = $next + 1;
+	// 		}
+			//3\4\5\6
+			if($next >= 7)
+			{
+				$nextServer = 4;
+			}
+			else
+			{
+				$nextServer = $next + 1;
+			}
+			//3\4\5\6\7
+	// 		if($next >= 4)
+	// 		{
+	// 			$nextServer = 0;
+	// 		}
+	// 		else
+	// 		{
+	// 			$nextServer = $next + 1;
+	// 		}
+			//3\4\5\6\7\8
+	// 		if($next >= 5)
+	// 		{
+	// 			$nextServer = 0;
+	// 		}
+	// 		else
+	// 		{
+	// 			$nextServer = $next + 1;
+	// 		}
+			$sql = "UPDATE `server_balance_check` SET `next_active` = 1 WHERE `server_id`={$nextServer}";
+			$productdb->query($sql);
+			$sql = "UPDATE `server_balance_check` SET `count` = 0, `next_active` = 0 WHERE `server_id`={$next}";
+			$productdb->query($sql);
 		}
 		else
 		{
-			$nextServer = $next + 1;
+			$sql = "UPDATE `server_balance_check` SET `count` = `count` + 1 WHERE `server_id`={$next}";
+			$productdb->query($sql);
 		}
-		//3\4\5\6\7
-// 		if($next >= 4)
-// 		{
-// 			$nextServer = 0;
-// 		}
-// 		else
-// 		{
-// 			$nextServer = $next + 1;
-// 		}
-		//3\4\5\6\7\8
-// 		if($next >= 5)
-// 		{
-// 			$nextServer = 0;
-// 		}
-// 		else
-// 		{
-// 			$nextServer = $next + 1;
-// 		}
-
-		$sql = "UPDATE `server_balance_check` SET `next_active` = 1 WHERE `server_id`={$nextServer}";
-		$productdb->query($sql);
-		$sql = "UPDATE `server_balance_check` SET `next_active` = 0 WHERE `server_id`={$next}";
-		$productdb->query($sql);
 		
 		$announcement = $this->config->item('game_announcement');
 		$jsonData = array_merge($jsonData, $announcement);
