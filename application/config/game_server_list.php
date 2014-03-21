b473ec6b8cfef60e48ceff76b91c159aaa9d7a07
johnnyeven_work <johnnyeven@gmail.com>
2014-03-21 10:28:50 +0800

    1

--- application/config/game_server_list.php
+++ application/config/game_server_list.php
@@ -8,6 +8,34 @@ $config ['game_server_list1'] = array (
 		'activate' => 0,
 		'server' => array (
 				array (
+						'id' => '13',
+						'game_id' => 'B',
+						'section_id' => '113',
+						'account_server_id' => '109',
+						'server_name' => '风暴剑圣(09)',
+						'server_ip' => array (
+								0 => array (
+										'ip' => '58.68.251.44:8091' 
+								)
+						),
+						'server_game_ip' => '58.68.251.43',
+						'game_message_ip' => '10.68.237.90:9899',
+						'server_max_player' => '100000',
+						'account_count' => '0',
+						'server_language' => '中文',
+						'server_sort' => '12',
+						'server_recommend' => '0',
+						'server_debug' => '0',
+						'partner' => 'default,default_full,91,17173,pp,Downjoy,zq,uc',
+						'version' => '',
+						'server_status' => '1',
+						'server_new' => '1',
+						'special_ip' => '',
+						'need_activate' => '0',
+						'server_starttime' => '0',
+						'server_game_port' => '9999' 
+				),
+				array (
 						'id' => '12',
 						'game_id' => 'B',
 						'section_id' => '112',
@@ -239,6 +267,34 @@ $config ['game_server_list2'] = array (
 		'message' => 'SERVER_LIST_SUCCESS',
 		'activate' => 0,
 		'server' => array (
+				array (
+						'id' => '13',
+						'game_id' => 'B',
+						'section_id' => '113',
+						'account_server_id' => '109',
+						'server_name' => '风暴剑圣(09)',
+						'server_ip' => array (
+								0 => array (
+										'ip' => '58.68.251.44:8091' 
+								)
+						),
+						'server_game_ip' => '58.68.251.43',
+						'game_message_ip' => '10.68.237.90:9899',
+						'server_max_player' => '100000',
+						'account_count' => '0',
+						'server_language' => '中文',
+						'server_sort' => '12',
+						'server_recommend' => '0',
+						'server_debug' => '0',
+						'partner' => 'default,default_full,91,17173,pp,Downjoy,zq,uc',
+						'version' => '',
+						'server_status' => '1',
+						'server_new' => '1',
+						'special_ip' => '',
+						'need_activate' => '0',
+						'server_starttime' => '0',
+						'server_game_port' => '9999' 
+				),
 				array (
 						'id' => '12',
 						'game_id' => 'B',
