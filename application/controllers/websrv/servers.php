e0a1a4a56effd47d1f9d59799c3fcdd0f102ef65
johnnyeven_work <johnnyeven@gmail.com>
2014-05-09 16:38:23 +0800

    1

--- application/controllers/websrv/servers.php
+++ application/controllers/websrv/servers.php
@@ -54,7 +54,7 @@ class Servers extends CI_Controller {
 			$this->get_temp_hd_list();
 			exit();
 		}
-		elseif($partner == 'arab_default')
+		elseif($partner == 'arab_default' || $partner == 'arab_sdk')
 		{
 			$this->get_sdk_debug_list('96');
 			exit();
