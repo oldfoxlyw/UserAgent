2742f209a281c4bec077c27220b96bae392ae423
johnnyeven_work <johnnyeven@gmail.com>
2014-05-14 11:00:50 +0800

    1

--- application/controllers/websrv/servers_new.php
+++ application/controllers/websrv/servers_new.php
@@ -10,6 +10,24 @@ class Servers_new extends CI_Controller {
 	
 	public function server_list($format = 'json')
 	{
+		$partner	=	$this->input->get_post('partner', TRUE);
+		$mode		=	$this->input->get_post('mode', TRUE);
+		$lang		=	$this->input->get_post('language', TRUE);
+		$ver		=	$this->input->get_post('client_version', TRUE);
 
+		if($mode == 'pub')
+		$this->load->config('game_server_list');
+		
+		$serverIp	=	$this->input->server('SERVER_ADDR');
+		if($serverIp == '122.13.131.55')
+		{
+			//3\4\5\6\7\8
+			$jsonData = $this->config->item('game_server_list2');
+		}
+		else //183.60.255.55
+		{
+			//3\4\5\6\7\8
+			$jsonData = $this->config->item('game_server_list1');
+		}
 	}
 }
