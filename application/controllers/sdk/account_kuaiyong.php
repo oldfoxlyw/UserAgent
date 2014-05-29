52e7198dd573f4a15f0f275ecfbcd6d8e83e3a34
johnnyeven_work <johnnyeven@gmail.com>
2014-05-29 13:46:29 +0800

    1

--- application/controllers/sdk/account_kuaiyong.php
+++ application/controllers/sdk/account_kuaiyong.php
@@ -237,7 +237,8 @@ class Account_kuaiyong extends CI_Controller
 							'log_action'	=>	'ACCOUNT_REGISTER_SUCCESS',
 							'account_guid'	=>	$user->GUID,
 							'account_name'	=>	$user->account_name,
-							'server_id'		=>	$server_id
+							'server_id'		=>	$server_id,
+							'partner_key'	=>	$partner_key
 					);
 					$this->logs->write($logParameter);
 				}
