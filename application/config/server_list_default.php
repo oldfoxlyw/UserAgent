1dd70eba54fbc6a334dfe28a8f67675d17abf992
johnnyeven_work <johnnyeven@gmail.com>
2014-06-06 16:22:22 +0800

    1

--- application/config/server_list_default.php
+++ application/config/server_list_default.php
@@ -20,6 +20,8 @@ $config ['game_server_list1'] = array (
 						),
 						'server_game_ip' => '85.195.100.236',
 						'game_message_ip' => '10.11.12.15:9899',
+						'const_server_ip' => '85.195.100.234:8091',
+						'voice_server_ip' => '85.195.100.236:8099',
 						'server_max_player' => '100000',
 						'account_count' => '0',
 						'server_language' => '',
