d70d5d778f546a5f09de18c230894b403d6c431b
johnnyeven_work <johnnyeven@gmail.com>
2014-06-09 13:05:34 +0800

    1

--- application/config/config.php
+++ application/config/config.php
@@ -1,5 +1,5 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-date_default_timezone_set('PRC');
+date_default_timezone_set('Asia/Hong_Kong');
 
 /*
 |--------------------------------------------------------------------------
