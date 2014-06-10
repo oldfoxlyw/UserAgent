1a4b56cb31ea6071d166e4228f05ab3fc265450f
johnnyeven_work <johnnyeven@gmail.com>
2014-06-10 10:47:22 +0800

    1

--- application/config/server_list_default.php
+++ application/config/server_list_default.php
@@ -7,66 +7,66 @@ $config ['game_server_list1'] = array (
 		'message' => 'SERVER_LIST_SUCCESS',
 		'activate' => 0,
 		'server' => array (
-				// array (
-				// 		'id' => '1',
-				// 		'game_id' => 'B',
-				// 		'section_id' => '403',
-				// 		'account_server_id' => '403',
-				// 		'server_name' => '独眼巨人',
-				// 		'server_ip' => array (
-				// 				0 => array (
-				// 						'ip' => '218.213.235.125:8091' 
-				// 				)
-				// 		),
-				// 		'server_game_ip' => '218.213.235.121',
-				// 		'game_message_ip' => '10.11.12.3:9899',
-				// 		'const_server_ip' => '218.213.235.125:8091',
-				// 		'voice_server_ip' => '218.213.235.125:8088',
-				// 		'server_max_player' => '100000',
-				// 		'account_count' => '0',
-				// 		'server_language' => '中文',
-				// 		'server_sort' => '3',
-				// 		'server_recommend' => '1',
-				// 		'server_debug' => '0',
-				// 		'partner' => 'tw_default,tw_facebook',
-				// 		'version' => '',
-				// 		'server_status' => '1',
-				// 		'server_new' => '1',
-				// 		'special_ip' => '',
-				// 		'need_activate' => '0',
-				// 		'server_starttime' => '0',
-				// 		'server_game_port' => '9999' 
-				// ),
-				// array (
-				// 		'id' => '1',
-				// 		'game_id' => 'B',
-				// 		'section_id' => '402',
-				// 		'account_server_id' => '402',
-				// 		'server_name' => '静月斥候',
-				// 		'server_ip' => array (
-				// 				0 => array (
-				// 						'ip' => '218.213.235.124:8091' 
-				// 				)
-				// 		),
-				// 		'server_game_ip' => '218.213.235.120',
-				// 		'game_message_ip' => '10.11.12.2:9899',
-				// 		'const_server_ip' => '218.213.235.124:8091',
-				// 		'voice_server_ip' => '218.213.235.124:8088',
-				// 		'server_max_player' => '100000',
-				// 		'account_count' => '0',
-				// 		'server_language' => '中文',
-				// 		'server_sort' => '2',
-				// 		'server_recommend' => '0',
-				// 		'server_debug' => '0',
-				// 		'partner' => 'tw_default,tw_facebook',
-				// 		'version' => '',
-				// 		'server_status' => '1',
-				// 		'server_new' => '1',
-				// 		'special_ip' => '',
-				// 		'need_activate' => '0',
-				// 		'server_starttime' => '0',
-				// 		'server_game_port' => '9999' 
-				// ),
+				array (
+						'id' => '1',
+						'game_id' => 'B',
+						'section_id' => '403',
+						'account_server_id' => '403',
+						'server_name' => '独眼巨人',
+						'server_ip' => array (
+								0 => array (
+										'ip' => '218.213.235.125:8091' 
+								)
+						),
+						'server_game_ip' => '218.213.235.121',
+						'game_message_ip' => '10.11.12.3:9899',
+						'const_server_ip' => '218.213.235.125:8091',
+						'voice_server_ip' => '218.213.235.125:8088',
+						'server_max_player' => '100000',
+						'account_count' => '0',
+						'server_language' => '中文',
+						'server_sort' => '3',
+						'server_recommend' => '1',
+						'server_debug' => '0',
+						'partner' => 'tw_default,tw_facebook',
+						'version' => '',
+						'server_status' => '1',
+						'server_new' => '1',
+						'special_ip' => '',
+						'need_activate' => '0',
+						'server_starttime' => '0',
+						'server_game_port' => '9999' 
+				),
+				array (
+						'id' => '1',
+						'game_id' => 'B',
+						'section_id' => '402',
+						'account_server_id' => '402',
+						'server_name' => '静月斥候',
+						'server_ip' => array (
+								0 => array (
+										'ip' => '218.213.235.124:8091' 
+								)
+						),
+						'server_game_ip' => '218.213.235.120',
+						'game_message_ip' => '10.11.12.2:9899',
+						'const_server_ip' => '218.213.235.124:8091',
+						'voice_server_ip' => '218.213.235.124:8088',
+						'server_max_player' => '100000',
+						'account_count' => '0',
+						'server_language' => '中文',
+						'server_sort' => '2',
+						'server_recommend' => '0',
+						'server_debug' => '0',
+						'partner' => 'tw_default,tw_facebook',
+						'version' => '',
+						'server_status' => '1',
+						'server_new' => '1',
+						'special_ip' => '',
+						'need_activate' => '0',
+						'server_starttime' => '0',
+						'server_game_port' => '9999' 
+				),
 				array (
 						'id' => '1',
 						'game_id' => 'B',
