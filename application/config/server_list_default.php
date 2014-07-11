0d868558d2c4bbb08ee29028b55fae6d43245d1f
johnnyeven_work <johnnyeven@gmail.com>
2014-07-11 16:46:31 +0800

    1

--- application/config/server_list_default.php
+++ application/config/server_list_default.php
@@ -10,6 +10,36 @@ $config ['game_server_list1'] = array (
 				array (
 						'id' => '1',
 						'game_id' => 'B',
+						'section_id' => '402',
+						'account_server_id' => '402',
+						'server_name' => '靜月斥候（02）',
+						'server_ip' => array (
+								0 => array (
+										'ip' => '218.213.235.124:8091' 
+								)
+						),
+						'server_game_ip' => '218.213.235.120',
+						'game_message_ip' => '10.11.12.2:9899',
+						'const_server_ip' => '218.213.235.124:8091',
+						'voice_server_ip' => '218.213.235.124:8088',
+						'server_max_player' => '100000',
+						'account_count' => '0',
+						'server_language' => '中文',
+						'server_sort' => '2',
+						'server_recommend' => '1',
+						'server_debug' => '0',
+						'partner' => 'tw_default,tw_facebook',
+						'version' => '',
+						'server_status' => '1',
+						'server_new' => '1',
+						'special_ip' => '',
+						'need_activate' => '0',
+						'server_starttime' => '0',
+						'server_game_port' => '9999' 
+				),
+				array (
+						'id' => '1',
+						'game_id' => 'B',
 						'section_id' => '403',
 						'account_server_id' => '403',
 						'server_name' => '獨眼巨人（01）',
@@ -26,7 +56,7 @@ $config ['game_server_list1'] = array (
 						'account_count' => '0',
 						'server_language' => '中文',
 						'server_sort' => '3',
-						'server_recommend' => '1',
+						'server_recommend' => '0',
 						'server_debug' => '0',
 						'partner' => 'tw_default,tw_facebook',
 						'version' => '',
@@ -37,36 +67,6 @@ $config ['game_server_list1'] = array (
 						'server_starttime' => '0',
 						'server_game_port' => '9999' 
 				),
-				// array (
-				// 		'id' => '1',
-				// 		'game_id' => 'B',
-				// 		'section_id' => '402',
-				// 		'account_server_id' => '402',
-				// 		'server_name' => '靜月斥候（02）',
-				// 		'server_ip' => array (
-				// 				0 => array (
-				// 						'ip' => '218.213.235.124:8091' 
-				// 				)
-				// 		),
-				// 		'server_game_ip' => '218.213.235.120',
-				// 		'game_message_ip' => '10.11.12.2:9899',
-				// 		'const_server_ip' => '218.213.235.124:8091',
-				// 		'voice_server_ip' => '218.213.235.124:8088',
-				// 		'server_max_player' => '100000',
-				// 		'account_count' => '0',
-				// 		'server_language' => '中文',
-				// 		'server_sort' => '2',
-				// 		'server_recommend' => '0',
-				// 		'server_debug' => '0',
-				// 		'partner' => 'tw_default,tw_facebook',
-				// 		'version' => '',
-				// 		'server_status' => '1',
-				// 		'server_new' => '1',
-				// 		'special_ip' => '',
-				// 		'need_activate' => '0',
-				// 		'server_starttime' => '0',
-				// 		'server_game_port' => '9999' 
-				// ),
 				// array (
 				// 		'id' => '1',
 				// 		'game_id' => 'B',
