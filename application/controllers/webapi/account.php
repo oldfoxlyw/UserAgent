f422dfc84d37f799214042ae2ece6dcefdfa25e7
johnnyeven_work <johnnyeven@gmail.com>
2014-07-15 13:45:27 +0800

    1

--- application/controllers/webapi/account.php
+++ application/controllers/webapi/account.php
@@ -687,7 +687,7 @@ class Account extends CI_Controller {
 		for($i=0; $i<strlen($result->account_nickname); $i++)
 		{
 			$ch = substr($result->account_nickname, $i, 1);
-			echo ord(' ');
+			echo $ch . ', ' . ord($ch);
 			echo '<br>';
 		}
 		// echo $result->account_nickname;
