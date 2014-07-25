ebdef2f96602860c552cc3ecfc8071ef286b296e
johnnyeven_work <johnnyeven@gmail.com>
2014-07-25 10:28:24 +0800

    1

--- application/config/config.php
+++ application/config/config.php
@@ -1,5 +1,5 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-date_default_timezone_set('Etc/GMT-4');
+date_default_timezone_set('Etc/GMT+3');
 
 /*
 |--------------------------------------------------------------------------
