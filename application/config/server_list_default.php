fd72a9fa27f90dbe643aecdad14c1c8513f5aeb6
johnnyeven_work <johnnyeven@gmail.com>
2014-08-11 10:50:42 +0800

    1

--- application/config/server_list_default.php
+++ application/config/server_list_default.php
@@ -20,11 +20,15 @@ $config ['game_server_list'] = array (
 						),
 						'server_game_ip' => '58.68.251.50',
 						'game_message_ip' => '10.68.237.97:9977',
+						'const_server_ip' => '58.68.251.50:6091',
+						'voice_server_ip' => '58.68.251.50:6099',
+						'cross_server_ip' => '58.68.251.50:6091',
+						'legion_message_ip' => '10.68.237.97:1101',
 						'server_max_player' => '100000',
 						'account_count' => '0',
 						'server_language' => '中文',
 						'server_sort' => '12',
-						'server_recommend' => '0',
+						'server_recommend' => '1',
 						'server_debug' => '0',
 						'partner' => 'default,default_full',
 						'version' => '',
@@ -36,18 +40,22 @@ $config ['game_server_list'] = array (
 						'server_game_port' => '9988' 
 				),
 				array (
-						'id' => '12',
+						'id' => '2',
 						'game_id' => 'B',
-						'section_id' => '112',
-						'account_server_id' => '108',
-						'server_name' => '混沌龙神(08)',
+						'section_id' => '402',
+						'account_server_id' => '402',
+						'server_name' => 'Android02',
 						'server_ip' => array (
 								0 => array (
-										'ip' => '58.68.251.53:8091' 
+										'ip' => '58.68.251.52:6091' 
 								)
 						),
-						'server_game_ip' => '58.68.251.54',
-						'game_message_ip' => '10.68.237.101:9899',
+						'server_game_ip' => '58.68.251.52',
+						'game_message_ip' => '10.68.237.99:9977',
+						'const_server_ip' => '58.68.251.52:6091',
+						'voice_server_ip' => '58.68.251.52:6099',
+						'cross_server_ip' => '58.68.251.52:6091',
+						'legion_message_ip' => '10.68.237.99:1101',
 						'server_max_player' => '100000',
 						'account_count' => '0',
 						'server_language' => '中文',
@@ -61,7 +69,7 @@ $config ['game_server_list'] = array (
 						'special_ip' => '',
 						'need_activate' => '0',
 						'server_starttime' => '0',
-						'server_game_port' => '9999' 
+						'server_game_port' => '9988' 
 				)
 		)
 );
