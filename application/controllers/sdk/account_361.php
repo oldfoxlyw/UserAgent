a20d6a56c5b90912612578d670c297fbdb75c79a
johnnyeven_work <johnnyeven@gmail.com>
2014-08-12 11:32:45 +0800

    1

--- application/controllers/sdk/account_361.php
+++ application/controllers/sdk/account_361.php
@@ -62,7 +62,6 @@ class Account_361 extends CI_Controller
 						'where_in'	=>	array('server_id', $where_in)
 				);
 				$result = $this->web_account->read($parameter, $extension);
-				exit($this->web_account->db()->last_query());
 				if(empty($result))
 				{
 					$result = array();
