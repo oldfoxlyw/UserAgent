8af8c88fef037684d387b95a86fab85e5ae1cf97
johnnyeven_work <johnnyeven@gmail.com>
2014-08-13 14:41:01 +0800

    1

--- application/controllers/websrv/coupon.php
+++ application/controllers/websrv/coupon.php
@@ -81,16 +81,6 @@ class Coupon extends CI_Controller
 		}
 	}
 
-	public function test_dec()
-	{
-		$master_id = 164999758611433;
-		echo 'before: ' . $master_id . "\n";
-		$master_id = dechex($master_id);
-		echo 'hexed: ' . $master_id . ', string: ' . strval($master_id) . "\n";
-		$server_id = substr(strval($master_id), 0, 2);
-		echo 'id: ' . $server_id;
-	}
-
 	public function use_coupon()
 	{
 		$coupon = $this->input->get_post('coupon');
