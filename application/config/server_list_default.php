02cfd4a5afabd8c7a06a2410521d2c6518b8e104
johnnyeven_work <johnnyeven@gmail.com>
2014-08-13 14:14:21 +0800

    1

--- application/config/server_list_default.php
+++ application/config/server_list_default.php
@@ -15,13 +15,13 @@ $config ['game_server_list1'] = array (
 				// 		'server_name' => 'أسطورة الثلوج',
 				// 		'server_ip' => array (
 				// 				0 => array (
-				// 						'ip' => '85.195.100.234:8091' 
+				// 						'ip' => '85.195.100.234:6091' 
 				// 				)
 				// 		),
 				// 		'server_game_ip' => '85.195.100.236',
-				// 		'game_message_ip' => '10.11.12.15:9899',
-				// 		'const_server_ip' => '85.195.100.234:8091',
-				// 		'voice_server_ip' => '85.195.100.236:8088',
+				// 		'game_message_ip' => '10.11.12.15:8898',
+				// 		'const_server_ip' => '85.195.100.234:6091',
+				// 		'voice_server_ip' => '85.195.100.236:6088',
 				// 		'server_max_player' => '100000',
 				// 		'account_count' => '0',
 				// 		'server_language' => '',
@@ -35,7 +35,7 @@ $config ['game_server_list1'] = array (
 				// 		'special_ip' => '',
 				// 		'need_activate' => '0',
 				// 		'server_starttime' => '0',
-				// 		'server_game_port' => '9999' 
+				// 		'server_game_port' => '8899' 
 				// ),
 				array (
 						'id' => '1',
