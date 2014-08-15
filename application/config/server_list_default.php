ac4c9f4b23b6491408ea1f47369d873b81f930db
johnnyeven_work <johnnyeven@gmail.com>
2014-08-15 13:17:14 +0800

    1

--- application/config/server_list_default.php
+++ application/config/server_list_default.php
@@ -36,37 +36,37 @@ $config ['game_server_list1'] = array (
 						'need_activate' => '0',
 						'server_starttime' => '0',
 						'server_game_port' => '9999' 
+				),
+				array (
+						'id' => '2',
+						'game_id' => 'B',
+						'section_id' => '302',
+						'account_server_id' => '302',
+						'server_name' => 'أسطورة الثلوج',
+						'server_ip' => array (
+								0 => array (
+										'ip' => '85.195.100.234:6091' 
+								)
+						),
+						'server_game_ip' => '85.195.100.236',
+						'game_message_ip' => '10.11.12.15:8898',
+						'const_server_ip' => '85.195.100.234:6091',
+						'voice_server_ip' => '85.195.100.236:6088',
+						'server_max_player' => '100000',
+						'account_count' => '0',
+						'server_language' => '',
+						'server_sort' => '1',
+						'server_recommend' => '0',
+						'server_debug' => '0',
+						'partner' => 'arab_default,arab_sdk',
+						'version' => '',
+						'server_status' => '1',
+						'server_new' => '1',
+						'special_ip' => '',
+						'need_activate' => '0',
+						'server_starttime' => '0',
+						'server_game_port' => '8899' 
 				)
-				// array (
-				// 		'id' => '2',
-				// 		'game_id' => 'B',
-				// 		'section_id' => '302',
-				// 		'account_server_id' => '302',
-				// 		'server_name' => 'أسطورة الثلوج',
-				// 		'server_ip' => array (
-				// 				0 => array (
-				// 						'ip' => '85.195.100.234:6091' 
-				// 				)
-				// 		),
-				// 		'server_game_ip' => '85.195.100.236',
-				// 		'game_message_ip' => '10.11.12.15:8898',
-				// 		'const_server_ip' => '85.195.100.234:6091',
-				// 		'voice_server_ip' => '85.195.100.236:6088',
-				// 		'server_max_player' => '100000',
-				// 		'account_count' => '0',
-				// 		'server_language' => '',
-				// 		'server_sort' => '1',
-				// 		'server_recommend' => '0',
-				// 		'server_debug' => '0',
-				// 		'partner' => 'arab_default,arab_sdk',
-				// 		'version' => '',
-				// 		'server_status' => '1',
-				// 		'server_new' => '1',
-				// 		'special_ip' => '',
-				// 		'need_activate' => '0',
-				// 		'server_starttime' => '0',
-				// 		'server_game_port' => '8899' 
-				// )
 		)
 );
 
