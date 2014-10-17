bf0ba624b8325f4bfcede01c0916ae2236c43746
johnnyeven_work <johnnyeven@gmail.com>
2014-10-17 10:53:53 +0800

    1

--- application/controllers/cronjobs/message.php
+++ application/controllers/cronjobs/message.php
@@ -80,6 +80,8 @@ class Message extends CI_Controller
 										'content'			=>	$row->content
 								);
 								$data = $this->connector->post($ip . '/announcement', $parameter, FALSE);
+
+								log_message('error', $data);
 								
 	// 							$sql = "insert into debug(text)values('url=" . $ip . '/announcement, content=' . $row->content . ", return={$data}')";
 	// 							$this->web_account->db()->query($sql);
