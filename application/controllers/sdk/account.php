fe1beb653ea13f15979295ffc2de817daae0020e
johnnyeven_work <johnnyeven@gmail.com>
2014-10-17 14:31:01 +0800

    1

--- application/controllers/sdk/account.php
+++ application/controllers/sdk/account.php
@@ -17,7 +17,7 @@ class Account extends CI_Controller
 		$partner_key = $this->input->get_post('partner_key', TRUE);
 		$code = $this->input->get_post('code', TRUE);
 
-		log_message('error', 'parameter: uid=' . $uid . ', partner=' . $partner_key . ', code=' . $code);
+		// log_message('error', 'parameter: uid=' . $uid . ', partner=' . $partner_key . ', code=' . $code);
 		
 		if(empty($uid) || empty($partner_key))
 		{
@@ -294,9 +294,9 @@ class Account extends CI_Controller
 				}
 			}
 			ksort($parameter);
-			//array_push($parameter, $this->check_code);
-			log_message('error', 'parameter: ' . json_encode($parameter));
-			log_message('error', 'str: ' . implode('', $parameter));
+			// array_push($parameter, $this->check_code);
+			// log_message('error', 'parameter: ' . json_encode($parameter));
+			// log_message('error', 'str: ' . implode('', $parameter));
 			$str = strtolower(md5(implode('', $parameter)));
 
 // 			$this->load->model('web_account');
