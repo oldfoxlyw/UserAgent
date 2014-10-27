cbfeb7f44d9284c61d0736b8941854b3f181f674
johnnyeven_work <johnnyeven@gmail.com>
2014-10-27 13:58:51 +0800

    1

--- application/config/server_list_default.php
+++ application/config/server_list_default.php
@@ -10,25 +10,25 @@ $config ['game_server_list'] = array (
 				array (
 						'id' => '1',
 						'game_id' => 'B',
-						'section_id' => '401',
-						'account_server_id' => '401',
+						'section_id' => '601',
+						'account_server_id' => '601',
 						'server_name' => '红月之森(s19)',
 						'server_ip' => array (
 								0 => array (
-										'ip' => '58.68.251.50:6091' 
+										'ip' => '115.159.1.45:6091' 
 								)
 						),
 						'server_game_ip' => array(
 								0 => array (
-										'ip' 	=> 	'58.68.251.50',
+										'ip' 	=> 	'115.159.3.100',
 										'port' 	=>	'9988'
 								)
 						),
-						'game_message_ip' => '10.68.237.97:9977',
-						'const_server_ip' => '58.68.251.50:6091',
-						'voice_server_ip' => '58.68.251.50:8099',
-						'cross_server_ip' => '58.68.251.50:6091',
-						'legion_message_ip' => '10.68.237.97:1101',
+						'game_message_ip' => '10.247.27.130:9977',
+						'const_server_ip' => '115.159.1.45:6091',
+						'voice_server_ip' => '115.159.1.45:8099',
+						'cross_server_ip' => '115.159.1.45:6091',
+						'legion_message_ip' => '10.247.27.130:1101',
 						'server_max_player' => '100000',
 						'account_count' => '0',
 						'server_language' => '中文',
