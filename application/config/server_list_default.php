76c795758d50af6118c9c538e50b01ed910c61e7
johnnyeven_work <johnnyeven@gmail.com>
2014-10-28 17:18:48 +0800

    1

--- application/config/server_list_default.php
+++ application/config/server_list_default.php
@@ -43,42 +43,42 @@ $config ['game_server_list'] = array (
 						'need_activate' => '0',
 						'server_starttime' => '0'
 				),
-				// array (
-				// 		'id' => '2',
-				// 		'game_id' => 'B',
-				// 		'section_id' => '602',
-				// 		'account_server_id' => '602',
-				// 		'server_name' => '水晶溪谷',
-				// 		'server_ip' => array (
-				// 				0 => array (
-				// 						'ip' => '115.159.3.100:8091' 
-				// 				)
-				// 		),
-				// 		'server_game_ip' => array(
-				// 				0 => array (
-				// 						'ip' 	=> 	'115.159.1.45',
-				// 						'port' 	=>	'9999'
-				// 				)
-				// 		),
-				// 		'game_message_ip' => '10.247.41.87:9899',
-				// 		'const_server_ip' => '115.159.3.100:8091',
-				// 		'voice_server_ip' => '115.159.3.100:8099',
-				// 		'cross_server_ip' => '115.159.3.100:8091',
-				// 		'legion_message_ip' => '10.247.41.87:1101',
-				// 		'server_max_player' => '100000',
-				// 		'account_count' => '0',
-				// 		'server_language' => '中文',
-				// 		'server_sort' => '10',
-				// 		'server_recommend' => '0',
-				// 		'server_debug' => '0',
-				// 		'partner' => 'default,default_full',
-				// 		'version' => '',
-				// 		'server_status' => '1',
-				// 		'server_new' => '1',
-				// 		'special_ip' => '',
-				// 		'need_activate' => '0',
-				// 		'server_starttime' => '0'
-				// )
+				array (
+						'id' => '2',
+						'game_id' => 'B',
+						'section_id' => '602',
+						'account_server_id' => '602',
+						'server_name' => '水晶溪谷',
+						'server_ip' => array (
+								0 => array (
+										'ip' => '115.159.3.100:8091' 
+								)
+						),
+						'server_game_ip' => array(
+								0 => array (
+										'ip' 	=> 	'115.159.1.45',
+										'port' 	=>	'9999'
+								)
+						),
+						'game_message_ip' => '10.247.41.87:9899',
+						'const_server_ip' => '115.159.3.100:8091',
+						'voice_server_ip' => '115.159.3.100:8099',
+						'cross_server_ip' => '115.159.3.100:8091',
+						'legion_message_ip' => '10.247.41.87:1101',
+						'server_max_player' => '100000',
+						'account_count' => '0',
+						'server_language' => '中文',
+						'server_sort' => '10',
+						'server_recommend' => '0',
+						'server_debug' => '0',
+						'partner' => 'default,default_full',
+						'version' => '',
+						'server_status' => '1',
+						'server_new' => '1',
+						'special_ip' => '',
+						'need_activate' => '0',
+						'server_starttime' => '0'
+				)
 		)
 );
 
