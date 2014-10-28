559de5cb75c547795d533225bcd5cc1a8b31dd1e
johnnyeven_work <johnnyeven@gmail.com>
2014-10-28 16:20:16 +0800

    1

--- application/config/server_list_default.php
+++ application/config/server_list_default.php
@@ -46,32 +46,32 @@ $config ['game_server_list'] = array (
 				// array (
 				// 		'id' => '2',
 				// 		'game_id' => 'B',
-				// 		'section_id' => '402',
-				// 		'account_server_id' => '402',
-				// 		'server_name' => '恶火狼王',
+				// 		'section_id' => '602',
+				// 		'account_server_id' => '602',
+				// 		'server_name' => '水晶溪谷',
 				// 		'server_ip' => array (
 				// 				0 => array (
-				// 						'ip' => '58.68.251.52:6091' 
+				// 						'ip' => '115.159.3.100:8091' 
 				// 				)
 				// 		),
 				// 		'server_game_ip' => array(
 				// 				0 => array (
-				// 						'ip' 	=> 	'58.68.251.52',
-				// 						'port' 	=>	'9988'
+				// 						'ip' 	=> 	'115.159.1.45',
+				// 						'port' 	=>	'9999'
 				// 				)
 				// 		),
-				// 		'game_message_ip' => '10.68.237.99:9977',
-				// 		'const_server_ip' => '58.68.251.52:6091',
-				// 		'voice_server_ip' => '58.68.251.52:8099',
-				// 		'cross_server_ip' => '58.68.251.52:6091',
-				// 		'legion_message_ip' => '10.68.237.99:1101',
+				// 		'game_message_ip' => '10.247.41.87:9899',
+				// 		'const_server_ip' => '115.159.3.100:8091',
+				// 		'voice_server_ip' => '115.159.3.100:8099',
+				// 		'cross_server_ip' => '115.159.3.100:8091',
+				// 		'legion_message_ip' => '10.247.41.87:1101',
 				// 		'server_max_player' => '100000',
 				// 		'account_count' => '0',
 				// 		'server_language' => '中文',
 				// 		'server_sort' => '10',
 				// 		'server_recommend' => '0',
 				// 		'server_debug' => '0',
-				// 		'partner' => 'default,default_full,91,17173,pp,Downjoy,zq,uc',
+				// 		'partner' => 'default,default_full',
 				// 		'version' => '',
 				// 		'server_status' => '1',
 				// 		'server_new' => '1',
