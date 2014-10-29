a3f69035c16a2d5d11dcb67785c7b4f13c2be528
johnnyeven_work <johnnyeven@gmail.com>
2014-10-29 17:01:17 +0800

    1

--- application/controllers/websrv/servers.php
+++ application/controllers/websrv/servers.php
@@ -296,8 +296,7 @@ class Servers extends CI_Controller {
 			'announce'			=>	$announce
 		);
 		$result = $this->return_format->format($jsonData, 'json');
-		str_replace('id', '', $result);
-		echo $result;
+		echo str_replace('\r', '', $result);
 	}
 }
 ?>
