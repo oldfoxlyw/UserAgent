dc0181ee27f12533a2a8d8be459b02fb3735c347
johnnyeven_work <johnnyeven@gmail.com>
2014-11-05 10:36:25 +0800

    1

--- application/config/server_list_default.php
+++ application/config/server_list_default.php
@@ -37,36 +37,36 @@ $config ['game_server_list1'] = array (
 						'server_starttime' => '0',
 						'server_game_port' => '8899' 
 				),
-				array (
-						'id' => '1',
-						'game_id' => 'B',
-						'section_id' => '301',
-						'account_server_id' => '301',
-						'server_name' => 'نور الفجر',
-						'server_ip' => array (
-								0 => array (
-										'ip' => '85.195.100.234:8091' 
-								)
-						),
-						'server_game_ip' => '85.195.100.236',
-						'game_message_ip' => '10.11.12.15:9899',
-						'const_server_ip' => '85.195.100.234:8091',
-						'voice_server_ip' => '85.195.100.236:8088',
-						'server_max_player' => '100000',
-						'account_count' => '0',
-						'server_language' => '',
-						'server_sort' => '1',
-						'server_recommend' => '0',
-						'server_debug' => '0',
-						'partner' => 'arab_default,arab_sdk',
-						'version' => '',
-						'server_status' => '1',
-						'server_new' => '1',
-						'special_ip' => '',
-						'need_activate' => '0',
-						'server_starttime' => '0',
-						'server_game_port' => '9999' 
-				)
+				// array (
+				// 		'id' => '1',
+				// 		'game_id' => 'B',
+				// 		'section_id' => '301',
+				// 		'account_server_id' => '301',
+				// 		'server_name' => 'نور الفجر',
+				// 		'server_ip' => array (
+				// 				0 => array (
+				// 						'ip' => '85.195.100.234:8091' 
+				// 				)
+				// 		),
+				// 		'server_game_ip' => '85.195.100.236',
+				// 		'game_message_ip' => '10.11.12.15:9899',
+				// 		'const_server_ip' => '85.195.100.234:8091',
+				// 		'voice_server_ip' => '85.195.100.236:8088',
+				// 		'server_max_player' => '100000',
+				// 		'account_count' => '0',
+				// 		'server_language' => '',
+				// 		'server_sort' => '1',
+				// 		'server_recommend' => '0',
+				// 		'server_debug' => '0',
+				// 		'partner' => 'arab_default,arab_sdk',
+				// 		'version' => '',
+				// 		'server_status' => '1',
+				// 		'server_new' => '1',
+				// 		'special_ip' => '',
+				// 		'need_activate' => '0',
+				// 		'server_starttime' => '0',
+				// 		'server_game_port' => '9999' 
+				// )
 		)
 );
 
