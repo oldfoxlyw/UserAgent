1d55dcc06ed85b8a564f401c87edb69d42d61446
johnnyeven_work <johnnyeven@gmail.com>
2014-11-05 14:48:03 +0800

    1

--- application/config/server_list_default.php
+++ application/config/server_list_default.php
@@ -10,18 +10,18 @@ $config ['game_server_list1'] = array (
 				array (
 						'id' => '2',
 						'game_id' => 'B',
-						'section_id' => '801',
-						'account_server_id' => '801',
+						'section_id' => '803',
+						'account_server_id' => '803',
 						'server_name' => 'test',
 						'server_ip' => array (
 								0 => array (
-										'ip' => '85.195.100.234:6091' 
+										'ip' => '184.173.231.228:8091' 
 								)
 						),
 						'server_game_ip' => '85.195.100.236',
-						'game_message_ip' => '10.11.12.15:8898',
-						'const_server_ip' => '85.195.100.234:6091',
-						'voice_server_ip' => '85.195.100.236:6088',
+						'game_message_ip' => '10.11.12.15:9899',
+						'const_server_ip' => '184.173.231.228:8091',
+						'voice_server_ip' => '184.173.231.228:8091',
 						'server_max_player' => '100000',
 						'account_count' => '0',
 						'server_language' => '',
@@ -35,7 +35,7 @@ $config ['game_server_list1'] = array (
 						'special_ip' => '',
 						'need_activate' => '0',
 						'server_starttime' => '0',
-						'server_game_port' => '8899' 
+						'server_game_port' => '9999' 
 				)
 		)
 );
