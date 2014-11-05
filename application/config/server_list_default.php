2ebb546ef3ce10d0dfc9fe4c8e14118096b8c278
johnnyeven_work <johnnyeven@gmail.com>
2014-11-05 14:58:19 +0800

    1

--- application/config/server_list_default.php
+++ application/config/server_list_default.php
@@ -95,4 +95,7 @@ $config['game_announcement'] = array(
 				'partner_key' => 'default,default_full'
 		)
 );
+
+$config['game_version'] = 2;
+$config['game_download_url'] = 'http://shouji.360tpcdn.com/141029/a12783984efd4ca22c2e4fafc2f5ec7d/com.digiarty.org_1.apk';
 ?>
