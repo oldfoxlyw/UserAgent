5d504bbf30a2654b008cb53750bbf0c1c8ff1a33
johnnyeven_work <johnnyeven@gmail.com>
2014-11-28 14:45:16 +0800

    1

--- application/controllers/cronjobs/overview.php
+++ application/controllers/cronjobs/overview.php
@@ -683,7 +683,7 @@ class Overview extends CI_Controller
 						'level1'					=>	$level1
 				);
 				
-				//$this->logcachedb->insert('log_retention1', $parameter);
+				$this->logcachedb->insert('log_retention1', $parameter);
 			}
 			
 
@@ -859,7 +859,7 @@ class Overview extends CI_Controller
 					'seven_retention_huge'		=>	$sevenRetentionHuge,
 					'level1'					=>	$level1
 			);
-			//$this->logcachedb->insert('log_retention1', $parameter);
+			$this->logcachedb->insert('log_retention1', $parameter);
 		}
 
 		$this->load->model('mwebconfig');
