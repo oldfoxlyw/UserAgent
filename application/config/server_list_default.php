16512411cdfafab3ba62395f55758ca0c13da9d9
johnnyeven_work <johnnyeven@gmail.com>
2014-12-03 13:42:19 +0800

    1

--- application/config/server_list_default.php
+++ application/config/server_list_default.php
@@ -43,18 +43,14 @@ $config ['game_server_list1'] = array (
 $config['game_announcement'] = array(
 		'announce' => array (
 				'id' => '1',
-				'summary' => "Welcome to the world of Warthrone! 
+				'summary' => "Welcome to the world of Warthrone! If you have any problems or suggestions for the game please contact customer support at: warthronesupport@fedeen.com. 
 
 We've prepared plenty of events and gift packs, so don't forget to check out the events and package menus in the game. 
-
-If you have any problems or suggestions for the game please contact customer support at: warthronesupport@fedeen.com. 
 
 Good luck and enjoy your adventure.",
-				'content' => "Welcome to the world of Warthrone! 
+				'content' => "Welcome to the world of Warthrone! If you have any problems or suggestions for the game please contact customer support at: warthronesupport@fedeen.com. 
 
 We've prepared plenty of events and gift packs, so don't forget to check out the events and package menus in the game. 
-
-If you have any problems or suggestions for the game please contact customer support at: warthronesupport@fedeen.com. 
 
 Good luck and enjoy your adventure."
 		)
