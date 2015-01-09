ba074b6f3ace4e6ffc74f4ea965735bc149ad9fb
johnnyeven_work <johnnyeven@gmail.com>
2015-01-09 14:42:39 +0800

    1

--- application/config/server_list_default.php
+++ application/config/server_list_default.php
@@ -36,6 +36,36 @@ $config ['game_server_list1'] = array (
 						'need_activate' => '0',
 						'server_starttime' => '0',
 						'server_game_port' => '9999' 
+				)，
+				array (
+						'id' => '3',
+						'game_id' => 'B',
+						'section_id' => '802',
+						'account_server_id' => '802',
+						'server_name' => '审核服us02',
+						'server_ip' => array (
+								0 => array (
+										'ip' => '184.173.231.227:8091' 
+								)
+						),
+						'server_game_ip' => '184.173.231.228',
+						'game_message_ip' => '10.48.104.184:9899',
+						'const_server_ip' => '184.173.231.227:8091',
+						'voice_server_ip' => '184.173.231.227:8099',
+						'server_max_player' => '100000',
+						'account_count' => '0',
+						'server_language' => '',
+						'server_sort' => '1',
+						'server_recommend' => '0',
+						'server_debug' => '0',
+						'partner' => 'en_default,en_arc',
+						'version' => '',
+						'server_status' => '1',
+						'server_new' => '1',
+						'special_ip' => '',
+						'need_activate' => '0',
+						'server_starttime' => '0',
+						'server_game_port' => '9999' 
 				)
 		)
 );
