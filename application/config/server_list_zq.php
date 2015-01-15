93bfa881c44b2b912990aeea42f525413577a772
johnnyeven_work <johnnyeven@gmail.com>
2015-01-15 17:15:36 +0800

    1

---
+++ application/config/server_list_zq.php
@@ -0,0 +1,226 @@
+<?php
+if (! defined ( 'BASEPATH' ))
+	exit ( 'No direct script access allowed' );
+
+//电信列表
+$config ['game_server_list'] = array (
+		'message' => 'SERVER_LIST_SUCCESS',
+		'activate' => 0,
+		'server' => array (
+				array (
+						'id' => '18',
+						'game_id' => 'B',
+						'section_id' => '118',
+						'account_server_id' => '204',
+						'server_name' => '静月沼泽(s23)',
+						'server_ip' => array (
+								0 => array (
+										'ip' => '58.68.251.54:8091' 
+								)
+						),
+						'server_game_ip' => '58.68.251.53',
+						'game_message_ip' => '10.68.237.100:9899',
+						'const_server_ip' => '58.68.251.54:8091',
+						'voice_server_ip' => '58.68.251.54:8099',
+						'server_max_player' => '100000',
+						'account_count' => '0',
+						'server_language' => '中文',
+						'server_sort' => '5',
+						'server_recommend' => '1',
+						'server_debug' => '0',
+						'partner' => '91,17173,pp,Downjoy,zq,uc,Itools,arab_default,TongBuTui,kuaiyong',
+						'version' => '1.3',
+						'server_status' => '1',
+						'server_new' => '1',
+						'special_ip' => '',
+						'need_activate' => '0',
+						'server_starttime' => '0',
+						'server_game_port' => '9999' 
+				),
+				array (
+						'id' => '17',
+						'game_id' => 'B',
+						'section_id' => '117',
+						'account_server_id' => '203',
+						'server_name' => '阴风海角(s21)',
+						'server_ip' => array (
+								0 => array (
+										'ip' => '58.68.251.52:8091' 
+								)
+						),
+						'server_game_ip' => '58.68.251.51',
+						'game_message_ip' => '10.68.237.98:9899',
+						'const_server_ip' => '58.68.251.52:8091',
+						'voice_server_ip' => '58.68.251.52:8099',
+						'server_max_player' => '100000',
+						'account_count' => '0',
+						'server_language' => '中文',
+						'server_sort' => '4',
+						'server_recommend' => '0',
+						'server_debug' => '0',
+						'partner' => '91,17173,pp,Downjoy,zq,uc,Itools,arab_default,TongBuTui,kuaiyong',
+						'version' => '1.3',
+						'server_status' => '1',
+						'server_new' => '1',
+						'special_ip' => '',
+						'need_activate' => '0',
+						'server_starttime' => '0',
+						'server_game_port' => '9999' 
+				),
+				array (
+						'id' => '16',
+						'game_id' => 'B',
+						'section_id' => '116',
+						'account_server_id' => '202',
+						'server_name' => '潮汐女皇(s19)',
+						'server_ip' => array (
+								0 => array (
+										'ip' => '58.68.251.50:8091' 
+								)
+						),
+						'server_game_ip' => '58.68.251.49',
+						'game_message_ip' => '10.68.237.96:9899',
+						'const_server_ip' => '58.68.251.50:8091',
+						'voice_server_ip' => '58.68.251.50:8099',
+						'server_max_player' => '100000',
+						'account_count' => '0',
+						'server_language' => '中文',
+						'server_sort' => '3',
+						'server_recommend' => '0',
+						'server_debug' => '0',
+						'partner' => '91,17173,pp,Downjoy,zq,uc,Itools,arab_default,TongBuTui,kuaiyong',
+						'version' => '1.3',
+						'server_status' => '1',
+						'server_new' => '1',
+						'special_ip' => '',
+						'need_activate' => '0',
+						'server_starttime' => '0',
+						'server_game_port' => '9999' 
+				),
+				array (
+						'id' => '15',
+						'game_id' => 'B',
+						'section_id' => '115',
+						'account_server_id' => '201',
+						'server_name' => '恶魔王爵(s17)',
+						'server_ip' => array (
+								0 => array (
+										'ip' => '58.68.251.48:8091' 
+								)
+						),
+						'server_game_ip' => '58.68.251.47',
+						'game_message_ip' => '10.68.237.94:9899',
+						'const_server_ip' => '58.68.251.48:8091',
+						'voice_server_ip' => '58.68.251.48:8099',
+						'server_max_player' => '100000',
+						'account_count' => '0',
+						'server_language' => '中文',
+						'server_sort' => '2',
+						'server_recommend' => '0',
+						'server_debug' => '0',
+						'partner' => '91,17173,pp,Downjoy,zq,uc,Itools,arab_default,TongBuTui,kuaiyong',
+						'version' => '1.3',
+						'server_status' => '1',
+						'server_new' => '1',
+						'special_ip' => '',
+						'need_activate' => '0',
+						'server_starttime' => '0',
+						'server_game_port' => '9999' 
+				),
+				array (
+						'id' => '14',
+						'game_id' => 'B',
+						'section_id' => '114',
+						'account_server_id' => '200',
+						'server_name' => '毁灭幽影(s15)',
+						'server_ip' => array (
+								0 => array (
+										'ip' => '58.68.251.46:8091' 
+								)
+						),
+						'server_game_ip' => '58.68.251.45',
+						'game_message_ip' => '10.68.237.92:9899',
+						'const_server_ip' => '58.68.251.46:8091',
+						'voice_server_ip' => '58.68.251.46:8099',
+						'server_max_player' => '100000',
+						'account_count' => '0',
+						'server_language' => '中文',
+						'server_sort' => '1',
+						'server_recommend' => '0',
+						'server_debug' => '0',
+						'partner' => '91,17173,pp,Downjoy,zq,uc,Itools,arab_default,TongBuTui,kuaiyong',
+						'version' => '1.3',
+						'server_status' => '1',
+						'server_new' => '1',
+						'special_ip' => '',
+						'need_activate' => '0',
+						'server_starttime' => '0',
+						'server_game_port' => '9999' 
+				)
+		)
+);
+
+$config['game_announcement'] = array(
+		'announce' => array (
+				'id' => '1',
+				'summary' => '《冰火王座》版本号：1.2
+越狱上架，活动不停
+
+1.激情世界杯，集卡赢好礼,6月20日-6月25日之间登陆游戏,普通副本和BOSS副本随机掉落球星卡牌,凑齐可以换取含有大量绿钻和金币的大力神杯礼包哟!每天可兑换2次,千万不要错过啦!
+2.冰火世界杯，竞猜赢绿钻,6月20日-6月27日参与贴吧【冰火王座吧】活动，竞猜比赛结果，赢绿钻、金币。
+3.欢乐世界杯，充值大返利,6月20日-6月25日期间，玩家在这5日内充值绿钻达到一定数量，活动结束后就可获得相应档次的礼包。详情请参看冰火官网活动介绍。
+4.充值即送四星随从，以及配套VIP随从装备。
+5.新增等级礼包，升级绿钻送不停。
+6.多重活动，领取灵魂精华，随从华丽提升。
+
+                                              1.2全新版本内容预览
+
+1.副本组队界面以及Boss战场界面增加语音聊天功能，玩家可以按住喇叭按钮进行录音，松开按钮后录音将会发送给其他玩家。
+2.等级上限提升至50级
+3.50级沙漠副本正式开放，玩家可以通过“精灵-达纳苏斯”主城正中的穿界门进入该副本，开始新的挑战。
+4.新增各职业50级荣誉套装（橙色装备）
+5.新增50级装备，随从装备图纸
+6.炼金中新增5阶宝石的合成选项，玩家可以通过融合8个4阶宝石来炼制5阶宝石。
+7.新增随从融合，强化玩法
+8.全新的5星随从强势登场！狂暴角魔·海利翁，冰霜女巫·安婕莉拉将在商店中出售，原有女武神与炎魔随从暂停出售，但玩家仍有  机会通过绿钻抽取到它们。
+9.新增6阶附魔卷轴，并在众神炼狱中掉落。
+10.修复会长无法退出工会以及解散工会
+11.添加反作弊机制
+12.修复随从等级的bug，现在随从等级不能高于主角等级
+13.添加含附件邮件以及角色删除的二次确认
+
+
+部落的号角在呼唤，体内的兽血已沸腾!战斗吧，勇士!',
+				'content' => '《冰火王座》版本号：1.2
+越狱上架，活动不停
+
+1.激情世界杯，集卡赢好礼,6月20日-6月25日之间登陆游戏,普通副本和BOSS副本随机掉落球星卡牌,凑齐可以换取含有大量绿钻和金币的大力神杯礼包哟!每天可兑换2次,千万不要错过啦!
+2.冰火世界杯，竞猜赢绿钻,6月20日-6月27日参与贴吧【冰火王座吧】活动，竞猜比赛结果，赢绿钻、金币。
+3.欢乐世界杯，充值大返利,6月20日-6月25日期间，玩家在这5日内充值绿钻达到一定数量，活动结束后就可获得相应档次的礼包。详情请参看冰火官网活动介绍。
+4.充值即送四星随从，以及配套VIP随从装备。
+5.新增等级礼包，升级绿钻送不停。
+6.多重活动，领取灵魂精华，随从华丽提升。
+
+                                              1.2全新版本内容预览
+
+1.副本组队界面以及Boss战场界面增加语音聊天功能，玩家可以按住喇叭按钮进行录音，松开按钮后录音将会发送给其他玩家。
+2.等级上限提升至50级
+3.50级沙漠副本正式开放，玩家可以通过“精灵-达纳苏斯”主城正中的穿界门进入该副本，开始新的挑战。
+4.新增各职业50级荣誉套装（橙色装备）
+5.新增50级装备，随从装备图纸
+6.炼金中新增5阶宝石的合成选项，玩家可以通过融合8个4阶宝石来炼制5阶宝石。
+7.新增随从融合，强化玩法
+8.全新的5星随从强势登场！狂暴角魔·海利翁，冰霜女巫·安婕莉拉将在商店中出售，原有女武神与炎魔随从暂停出售，但玩家仍有  机会通过绿钻抽取到它们。
+9.新增6阶附魔卷轴，并在众神炼狱中掉落。
+10.修复会长无法退出工会以及解散工会
+11.添加反作弊机制
+12.修复随从等级的bug，现在随从等级不能高于主角等级
+13.添加含附件邮件以及角色删除的二次确认
+
+
+部落的号角在呼唤，体内的兽血已沸腾!战斗吧，勇士!',
+				'post_time' => '1394121601',
+				'partner_key' => 'default,default_full,91,17173,pp,Downjoy,zq,uc'
+		)
+);
+?>
