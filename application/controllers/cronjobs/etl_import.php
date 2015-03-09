7bece340116af226b1e501a69c1ceee72dd265ce
johnnyeven_work <johnnyeven@gmail.com>
2015-03-09 17:42:02 +0800

    1

--- application/controllers/cronjobs/etl_import.php
+++ application/controllers/cronjobs/etl_import.php
@@ -14,18 +14,41 @@ class Etl_import extends CI_Controller
 			$time = time() - 2 * 86400;
 			$date = date('Ymd', $time);
 		}
+		log_message('custom', '------------------------------------------------------');
+		log_message('custom', 'Start to ETL progress...');
+		log_message('custom', '------------------------------------------------------');
+		log_message('custom', 'Data: ' . $date);
+		log_message('custom', '------------------------------------------------------');
 
-		$fp = popen("tar -xzvf /var/dw/gold_log.log-{$date}", 'r');
-		// $fp = fopen('/var/dw/gold_log.log', 'r');
-		$database = $this->load->database('log_cachedb', TRUE);
-		while(!feof($fp))
+		$list = array(
+			'arena_score_log',
+			'blue_crystal_log',
+			'equipment_log',
+			'experience_log',
+			'gold_log',
+			'guild_contribution_log'
+			'item_log',
+			'retinue_log',
+			'role_info_log'
+		);
+		foreach($list as $item)
 		{
-			$sql = fgets($fp);
-			$result = $database->query($sql);
-			var_dump($result);
+			log_message('custom', 'Load ' . $item . '-' . $date);
+			$fp = popen("tar -xzvf /var/dw/{$item}-{$date}", 'r');
+			// $fp = fopen('/var/dw/gold_log.log', 'r');
+			$database = $this->load->database('log_cachedb', TRUE);
+			while(!feof($fp))
+			{
+				$sql = fgets($fp);
+				$result = $database->query($sql);
+				var_dump($result);
+			}
+			// fclose($fp);
+			pclose($fp);
 		}
-		// fclose($fp);
-		pclose($fp);
+		log_message('custom', '------------------------------------------------------');
+		log_message('custom', 'ETL progress complete.');
+		log_message('custom', '------------------------------------------------------');
 	}
 }
 
