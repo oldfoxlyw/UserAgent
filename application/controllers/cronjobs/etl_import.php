045b7283d7735ffc13be9697b2f451bbee8452f2
johnnyeven_work <johnnyeven@gmail.com>
2015-03-09 16:59:07 +0800

    1

--- application/controllers/cronjobs/etl_import.php
+++ application/controllers/cronjobs/etl_import.php
@@ -3,11 +3,20 @@ if (! defined ( 'BASEPATH' )) exit ( 'No direct script access allowed' );
 
 class Etl_import extends CI_Controller
 {
-	public function import()
+	public function import($date)
 	{
+		error_reporting(E_ALL);
+		ini_set("display_errors", 1);
 		set_time_limit(0);
 
-		$fp = fopen('/var/dw/gold_log.log', 'r');
+		if(empty($date))
+		{
+			$time = time() - 2 * 86400;
+			$date = date('Ymd', $time);
+		}
+
+		$fp = popen("tar -xzvf /var/dw/gold_log.log-{$date}", 'r');
+		// $fp = fopen('/var/dw/gold_log.log', 'r');
 		$database = $this->load->database('log_cachedb', TRUE);
 		while(!feof($fp))
 		{
@@ -15,7 +24,8 @@ class Etl_import extends CI_Controller
 			$result = $database->query($sql);
 			var_dump($result);
 		}
-		fclose($fp);
+		// fclose($fp);
+		pclose($fp);
 	}
 }
 
